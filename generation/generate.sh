767757b63cb908ee20fded527ce7e1ade2d4f834
Oliver A Ruebenacker <oliverr@broadinstitute.org>
2019-11-02 17:52:56 -0400

    Swagger codegen; some more setup.

--- generation/generate.sh
+++ generation/generate.sh
@@ -4,15 +4,9 @@ codegen_module_jar=$codegen_module_dir/target/sttp-swagger-codegen-1.0.0.jar
 temp_dir=~/tmp
 codegen_target_dir=~/git/starfire/generated
 swagger_file=~/git/starfire/generation/terra-api.yaml
-cd $codegen_module_dir || exit
-echo "Building and packaging sttp module"
-mvn clean package
-echo "Done with sttp module"
-ls -l $codegen_module_jar
 codegen_temp_dir=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX --tmpdir=$temp_dir)
 echo "Now generating code in temporary folder $codegen_temp_dir"
-java -cp $swagger_codegen_jar:codegen_module_jar io.swagger.codegen.Codegen -l sttp -i $swagger_file -o $codegen_dir
-echo "Done generating code, now trying to build from generated code"
-cd $codegen_dir || exit
-pwd
-sbt compile
+java -cp $swagger_codegen_jar:$codegen_module_jar io.swagger.codegen.Codegen -l sttp \
+        -i $swagger_file -o $codegen_temp_dir
+echo "Done generating code to $codegen_temp_dir, now syncing to $codegen_target_dir"
+# rsync
